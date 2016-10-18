3c933b329d29f8d48c9b9c8f21c12821f05f6ecc
Haoran Wang <haoranwang14@gmail.com>
2016-10-18 05:01:28 +0000

    all

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,18 @@
 class Movie < ActiveRecord::Base
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    def self.sorting(sort)
+        if sort==nil
+            Movie
+        else
+            Movie.order(sort)
+        end
+    end
     
+    def self.all_ratings 
+        @all_ratings
+    end
+    
+    def self.rating(rating)
+        Movie.where('rating in (?)', rating)
+    end
 end
